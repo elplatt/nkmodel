d14d0cb5e11fd229c8c51ff8c067468fcdc64a76
Edward L Platt <ed@elplatt.com>
2017-07-26 19:32:02 -0400

    Specify concern loci as array of arrays.

--- nk.py
+++ nk.py
@@ -50,14 +50,16 @@ class NK(object):
     def get_hillclimb_values(self, states, loci=None):
         '''Generate values for each state with each of `loci` flipped.'''
         if loci is None:
-            loci = self.loci
+            loci = [self.loci for state in states]
+        if len(loci) != len(states):
+            raise ValueError
         N = self.N
         base_locus_values = [0.0] * N
         result_states = []
         result_values = []
         depends_on = self.depends_on
         dependence = self.dependence
-        for state in states:
+        for state_num, state in enumerate(states):
             # Calculate value of each locus before hill climbing
             for n in self.loci:
                 label = tuple([state[i] for i in dependence[n]])
@@ -68,7 +70,7 @@ class NK(object):
                     self.values[n][label] = v
                 base_locus_values[n] = v
             # Alter each locus
-            for l in loci:
+            for l in loci[state_num]:
                 locus_values = list(base_locus_values)
                 s = list(state)
                 s[l] = 1 - s[l]
