8428b31b700ec91eb1d5ba320409aeba6c18e017
Edward L Platt <ed@elplatt.com>
2017-07-27 16:30:28 -0400

    Conflicting merge.

--- nk.py
+++ nk.py
@@ -47,12 +47,17 @@ class NK(object):
             results[state_num] = total_value / N
         return results
             
-    def get_hillclimb_values(self, states, loci=None):
+    def get_hillclimb_values(self, states, state_loci=None):
         '''Generate values for each state with each of `loci` flipped.'''
+<<<<<<< Updated upstream
         if loci is None:
             loci = [self.loci for state in states]
         if len(loci) != len(states):
             raise ValueError
+=======
+        if state_loci is None:
+            state_loci = [self.loci] * len(states)
+>>>>>>> Stashed changes
         N = self.N
         base_locus_values = [0.0] * N
         result_states = []
@@ -70,7 +75,11 @@ class NK(object):
                     self.values[n][label] = v
                 base_locus_values[n] = v
             # Alter each locus
+<<<<<<< Updated upstream
             for l in loci[state_num]:
+=======
+            for l in state_loci[state_num]:
+>>>>>>> Stashed changes
                 locus_values = list(base_locus_values)
                 s = list(state)
                 s[l] = 1 - s[l]
