bb511f13d7a44b911c69a4d9581c5ca9a734125b
Edward L Platt <ed@elplatt.com>
2020-05-20 13:55:35 -0400

    Update package.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nkmodel",
-    version="0.0.1",
+    version="0.0.2",
     author="Edward L Platt",
     author_email="ed@elplatt.com",
     description="Implementation of Kauffman NK Model",
